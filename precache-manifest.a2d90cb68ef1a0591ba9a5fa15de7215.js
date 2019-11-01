458a0b235211d43ca2140298d7f32e0327c7ae97
Joe Williams <josephscottwilliams@gmail.com>
2019-11-01 15:10:14 -0500

    Updates

---
+++ precache-manifest.a2d90cb68ef1a0591ba9a5fa15de7215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244fa93026d817e476f7700e3dabc0e7",
+    "url": "/StarWarsClickyGame/index.html"
+  },
+  {
+    "revision": "daf16edb02a0ff1ec9e8",
+    "url": "/StarWarsClickyGame/static/css/main.1acb6fed.chunk.css"
+  },
+  {
+    "revision": "2331fd1c432bb8bd1939",
+    "url": "/StarWarsClickyGame/static/js/2.550ab4f7.chunk.js"
+  },
+  {
+    "revision": "daf16edb02a0ff1ec9e8",
+    "url": "/StarWarsClickyGame/static/js/main.d622d852.chunk.js"
+  },
+  {
+    "revision": "8b0fa7243d2eddd3f74d",
+    "url": "/StarWarsClickyGame/static/js/runtime-main.9b9513dc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/StarWarsClickyGame/static/media/logo.25bf045c.svg"
+  }
+]);
